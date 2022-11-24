bcd12dba500e84b9560d5d1786c54aeb34620861
Brandon <brandon_noel@live.ca>
2022-11-24 06:00:33 -0330

    new features

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const parent = document.getElementById("grid");
 
 const children = parent.children;
+const childrenChild = parent.children.childrenChild;
 
 let index = -1;
 
+
 for (let i = 0; i < children.length; i++) {
+  let rand = Math.floor(Math.random() * 100) + 1;
+
+if (rand <= 50) {
+    console.log("less");
+    document.getElementById(children[i].id).style.backgroundColor = "white";
+}
+else {
+    console.log("greater");
+    document.getElementById(children[i].id).style.backgroundColor = "darkgrey";
+}
+
   if (children[i].id === "gridItem5") {
     index = i;
-    break;
   }
 }
 
 console.log(index); // 👉️ 2
+
+  const onClick = (event) => {
+      console.log(event.target.id);
+      document.getElementById(event.target.id).style.backgroundColor = "red";
+  }
+  window.addEventListener('click', onClick);
