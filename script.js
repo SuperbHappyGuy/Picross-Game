6f52fb5308b17f04b81fb23b817e40105803e26a
Brandon <brandon_noel@live.ca>
2022-11-25 18:15:04 -0330

    Added working row counter for top row

--- script.js
+++ script.js
@@ -5,25 +5,68 @@ const childrenChild = parent.children.childrenChild;
 
 let index = -1;
 
+var row1 = [];
+var rowString = [];
+let rowCount = 0;
+var rowStringResult = "";
 
 for (let i = 0; i < children.length; i++) {
   let rand = Math.floor(Math.random() * 100) + 1;
 
 if (rand <= 50) {
-    console.log("less");
+    //console.log("less");
     document.getElementById(children[i].id).style.backgroundColor = "white";
 }
 else {
-    console.log("greater");
+    //console.log("greater");
     document.getElementById(children[i].id).style.backgroundColor = "darkgrey";
 }
+}
+
+for (let i = 0; i < children.length; i++) {
+  if(document.getElementById(children[i].id).style.backgroundColor == "darkgrey") {
+    row1.push({yes: "yes"})
+  }
+  else {
+    row1.push({no: "no"})
+  }
 
   if (children[i].id === "gridItem5") {
-    index = i;
+    break;
+  }
+}
+  console.log(row1);
+
+for(let i = 0; i < row1.length; i++) {
+  if(row1[i].yes == "yes") {
+    rowCount++;
+  }
+  else {
+    if(rowCount != "0") {
+    rowString.push({result: "" + rowCount})
+    }
+    console.log(rowCount);
+    rowCount = 0;
   }
 }
 
-console.log(index); // 👉️ 2
+if(rowCount != "0") {
+  rowString.push({result: "" + rowCount})
+  }
+
+console.log(rowCount);
+console.log(rowString);
+
+for(let i = 0; i < rowString.length; i++) {
+  rowStringResult += rowString[i].result + " ";
+}
+
+if(rowStringResult == "") {
+  rowStringResult = "0";
+}
+
+console.log(rowStringResult);
+document.getElementById("row1").innerHTML = rowStringResult;
 
   const onClick = (event) => {
       console.log(event.target.id);
