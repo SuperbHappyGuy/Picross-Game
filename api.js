61d9548fd1a5f1ee007ed51a1ec61fc02d326003
Brandon <brandon_noel@live.ca>
2023-02-16 18:34:08 -0330

    Updating Comit

--- api.js
+++ api.js
@@ -1,17 +1,30 @@
-/*function start() {
+var tempBtn = 0;
+
+async function callAPI() {
     tempBtn++;
+
+    // https://keepthescore.co/board/pbetkdyruye/
+    let editId = 'cdbusybllie';
+    let publicId = 'igwfgfibper';
+
+    let publicurl = 'https://keepthescore.co/board/igwfgfibper/'
+    let editurl = 'https://keepthescore.co/api/cdbusybllie'
+    
+    let newPlayer = {
+        "name": "New guy 2"
+    };
+    let response = fetch(`${editurl}/player/`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+            'accept': '*/*',
+        },
+        body: JSON.stringify(newPlayer)
+    });
+    
+    console.log(await response.json());
     document.getElementById("Start").innerHTML = tempBtn;
-  }
+}
 
-  let url = 'https://keepthescore.co/api/smnwouivdfr/board/';
-
-  fetch(url)
-  .then(response => response.json())
-  .then(data => {
-      console.log(data);
-});
-
-myFunction().then(data => {
-    console.log(data);
-});*/
-
+callAPI();
